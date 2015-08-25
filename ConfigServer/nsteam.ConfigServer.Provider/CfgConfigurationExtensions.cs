a59b528c01c67487c25a2e71fa4b69c2e71b8a75
israelito3000 <igarcia@nearsolutions.net>
2015-08-24 20:50:55 -0400

    add provider for configuration services vNext

---
+++ ConfigServer/nsteam.ConfigServer.Provider/CfgConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.Framework.Configuration.Helper;
+using Microsoft.Framework.Internal;
+using Microsoft.Framework.Configuration;
+
+namespace nsteam.ConfigServer.Provider
+{
+    public static class CfgConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddConfigServer(this IConfigurationBuilder configuration)
+        {
+            return AddConfigServer(configuration, null);
+        }
+
+        public static IConfigurationBuilder AddConfigServer(this IConfigurationBuilder configuration, string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                configuration.Add(new CfgConfigurationSource());
+            else
+                configuration.Add(new CfgConfigurationSource(url));
+
+            return configuration;
+        }
+    }
+}
---
+++ ConfigServer/nsteam.ConfigServer.Provider/CfgConfigurationSource.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Framework.Configuration;
+using nsteam.ConfigServer.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nsteam.ConfigServer.Provider
+{
+    public class CfgConfigurationSource : ConfigurationSource
+    {
+        public string ServiceUrl { get; }
+
+        public CfgConfigurationSource() : this(null){}
+
+        public CfgConfigurationSource(string serviceurl)
+        {
+            ServiceUrl = serviceurl;
+        }
+
+        public override void Load()
+        {
+            ConfigService cfg = null;
+
+            if (string.IsNullOrEmpty(ServiceUrl))
+                cfg = new ConfigService();                
+            else
+                cfg = new ConfigService(ServiceUrl);
+
+            dynamic obj = cfg.GetTree();
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.Indented);
+            Data["CfgServerSettings"] = json;
+            //JsonConfigurationFileParser parser = new JsonConfigurationFileParser();
+            //Data = parser.Parse(json);
+        }
+
+    }
+}
